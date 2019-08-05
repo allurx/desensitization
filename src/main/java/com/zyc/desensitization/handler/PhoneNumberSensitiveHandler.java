7c277fc6e2cf756a80cf26d8b3378ee2cc867e3e
zyc <1343786337@qq.com>
2019-08-05 22:13:02 +0800

    完善注释

--- src/main/java/com/zyc/desensitization/handler/PhoneNumberSensitiveHandler.java
+++ src/main/java/com/zyc/desensitization/handler/PhoneNumberSensitiveHandler.java
@@ -5,7 +5,7 @@ import com.zyc.desensitization.annotation.PhoneNumberSensitive;
 
 /**
  * 手机号码敏感信息处理者。
- * 注意该类在擦除敏感信息时不会校验目标对象的合法性，请确保目标是合法的身份证号码，
+ * 注意该类在擦除敏感信息时不会校验目标对象的合法性，请确保目标对象是合法的身份证号码，
  * 否则会抛出任何有可能的 {@link RuntimeException}。
  *
  * @author zyc
