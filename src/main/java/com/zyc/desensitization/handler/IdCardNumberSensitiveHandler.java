3c72144b84dbe3550be732a7f70273b7715e5d2b
zyc <1343786337@qq.com>
2019-08-06 22:37:45 +0800

    完善注释

--- src/main/java/com/zyc/desensitization/handler/IdCardNumberSensitiveHandler.java
+++ src/main/java/com/zyc/desensitization/handler/IdCardNumberSensitiveHandler.java
@@ -5,6 +5,8 @@ import com.zyc.desensitization.annotation.IdCardNumberSensitive;
 
 /**
  * 身份证号码敏感信息处理者
+ * 注意该类在擦除敏感信息时不会校验目标对象的合法性，请确保目标对象是合法的身份证号码，
+ * 否则会抛出任何有可能的 {@link RuntimeException}。
  *
  * @author zyc
  */
