97fb85eaf936715f81e95d0460ecc3c63a8c86c1
zyc <1343786337@qq.com>
2019-12-15 23:51:35 +0800

    code review

--- src/main/java/red/zyc/desensitization/support/InstanceCreator.java
+++ src/main/java/red/zyc/desensitization/support/InstanceCreator.java
@@ -28,7 +28,7 @@ package red.zyc.desensitization.support;
  * 在上述的类Person定义中只存在一个有参构造器，不存在无参构造器，因此在程序运行期间只能确定
  * 一个有参构造器，但不知道传入什么参数来调用该构造器，最终程序会基于{@link sun.misc.Unsafe}
  * 来构造这个对象，但是可能会丢失一些必要的对象信息，所以建议在定义自己的类时为其添加一个无参构造器
- * 以便程序能够基于反射来初始化该对象，或者你也可以实现该实例创建器接口定义类Person的默认创建器：
+ * 以便程序能够基于反射来初始化该对象，或者你也可以实现实例创建器接口来定义类Person的默认创建器：
  * <pre>
  *     class PersonInstanceCreator implements InstanceCreator&lt;Person&gt; {
  *
@@ -39,8 +39,9 @@ package red.zyc.desensitization.support;
  *
  *     }
  * </pre>
- * 最后调用{@link InstanceCreators#register}方法注册该实例创建器，最终程序在运行期间就会基于这个
- * 注册的实例创建器来创建相应的对象实例。
+ * 然后调用{@link InstanceCreators#register}方法注册该实例创建器，最终程序在运行期间就会基于这个
+ * 注册的实例创建器来创建相应的对象实例。对于实例创建器来说每次调用{@link InstanceCreator#create()}
+ * 方法都应该返回一个新的对象，这个新对象就是原对象被脱敏后的对象。
  *
  * @param <T> 实例类型
  * @author zyc
